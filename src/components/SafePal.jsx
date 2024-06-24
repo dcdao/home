aa172e842cdc98e13b9b6b959ae8c35ef5794a9d
Hamid Roohi <hamidroohi71@gmail.com>
2024-06-24 02:58:08 -0400

    fix(safe pal):  update safe pal design and contents

--- src/components/SafePal.jsx
+++ src/components/SafePal.jsx
@@ -16,16 +16,16 @@ const SafePal = () => {
 
   return (
     <div className="bg-black rounded-[50px] min-h-[742px] px-[50px]">
-      <h2 className="text-white text-[120px]">SafePal</h2>
-      <p className="text-white text-[30px] mt-[30px]">
+      <h2 className="text-white text-[120px] font-bold">SafePal</h2>
+      <p className="text-white text-[30px] font-[600]">
         Making your crypto experience safe and fun.
       </p>
-      <div className="flex items-center w-full">
+      <div className="flex items-center justify-center w-full">
         {images.map((image, index) => (
           <div key={index} className="p-[30px] ">
             <animated.img
               style={useSpring({
-                width: index === activeIndex ? "600px" : "300px",
+                width: index === activeIndex ? "400px" : "300px",
               })}
               src={image.src}
               alt={image.alt}
@@ -35,17 +35,17 @@ const SafePal = () => {
             {index === activeIndex && (
               <div className="flex items-center justify-between">
                 <div className="flex items-center gap-[10px] justify-center">
-                  <button className="bg-[gray] rounded-[30px] px-[15px] py-[8px]">
+                  <button className="bg-[#FB3B94] text-[#fff] rounded-[30px] px-[15px] py-[8px] font-[500] hover:scale-[1.1] duration-300">
                     Test
                   </button>
-                  <button className="bg-[gray] rounded-[30px] px-[15px] py-[8px]">
+                  <button className="bg-[#FB3B94] text-[#fff] rounded-[30px] px-[15px] py-[8px] font-[500] hover:scale-[1.1] duration-300">
                     Test
                   </button>
-                  <button className="bg-[gray] rounded-[30px] px-[15px] py-[8px]">
+                  <button className="bg-[#FB3B94] text-[#fff] rounded-[30px] px-[15px] py-[8px] font-[500] hover:scale-[1.1] duration-300">
                     Test
                   </button>
                 </div>
-                <button className="bg-[white] rounded-[30px] px-[40px] py-[8px]">
+                <button className="bg-[white] rounded-[30px] px-[40px] py-[8px] font-[500] hover:scale-[1.1] duration-300">
                   View
                 </button>
               </div>
